b595508c501a18d821dbe6c0b700d8d29b44e3d6
AlSudar <47494628+AlSudar@users.noreply.github.com>
2025-11-01 11:48:28 +0300

    Revert "Добавляет миниатюры фотографий"

--- js/main.js
+++ js/main.js
@@ -1,8 +1,5 @@
 import { getPhotoArray } from './get-photo-objects.js';
-import { renderThumbnails } from './thumbnails-render.js';
 
 const photoData = getPhotoArray();
 
-// console.log(photoData);
-
-renderThumbnails(photoData, photoData.length);
+console.log(photoData);

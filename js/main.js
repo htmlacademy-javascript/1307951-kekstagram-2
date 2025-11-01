ff1a83f4f7892c7a78eebf19ecf280ad82dc701d
gerry2923 <mironova_plus@mail.ru>
2025-11-01 11:12:08 +0300

    Добавляет миниатюры фотографий

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 import { getPhotoArray } from './get-photo-objects.js';
+import { renderThumbnails } from './thumbnails-render.js';
 
 const photoData = getPhotoArray();
 
-console.log(photoData);
+// console.log(photoData);
+
+renderThumbnails(photoData, photoData.length);
